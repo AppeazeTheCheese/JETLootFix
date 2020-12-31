6ed78c6535a3f7bc5dcaf230f54b96cb95a78866
AppeazeTheCheese <yolosolodbaz@gmail.com>
2020-12-31 14:19:11 -0700

    Initial Commit

---
+++ src/cacheSettings.js
@@ -0,0 +1,35 @@
+exports.mod = (mod_info) => {
+	
+	var items = fileIO.readParsed(db.user.cache.items).data;
+	
+	let spawnChances = [];
+	let spawnFilters = [];
+	
+	var resolvedChancePath = internal.path.resolve(internal.path.join(__dirname, "../Config/SpawnChances.json"));
+	var resolvedFilterPath = internal.path.resolve(internal.path.join(__dirname, "../Config/ContainerSpawnFilters.json"));
+	
+	if(fileIO.exist(resolvedChancePath))
+		spawnChances = fileIO.readParsed(resolvedChancePath);
+	if(fileIO.exist(resolvedFilterPath))
+		spawnFilters = fileIO.readParsed(resolvedFilterPath);
+	
+	for(let item in spawnChances){
+		if(items[spawnChances[item].id]){
+			if(spawnChances[item].Rarity !== undefined){
+				items[spawnChances[item].id]._props.Rarity = spawnChances[item].Rarity;
+			}
+			if(spawnChances[item].SpawnChance !== undefined){
+				items[spawnChances[item].id]._props.SpawnChance = spawnChances[item].SpawnChance;
+			}
+		}
+	}
+	
+	for(let item in spawnFilters){
+		if(items[spawnFilters[item].container]._props.SpawnFilter !== undefined && spawnFilters[item].spawnFilter !== undefined){
+			items[spawnFilters[item].container]._props.SpawnFilter = spawnFilters[item].spawnFilter;
+		}
+	}
+	
+	fileIO.write(db.user.cache.items, items);
+	logger.logSuccess("[LootFix] Settings applied")
+}
